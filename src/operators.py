cbdb5861f1ee034950dc6dbbed324021d05033cb
Meir Elbaz <meire@flow-sec.com>
2025-10-01 15:38:32 +0300

    fix address_overlaps and add ipv6 support

--- src/operators.py
+++ src/operators.py
@@ -94,7 +94,79 @@ def address_overlaps(
                         "$gte": Int64(address.broadcast_address)
                     },
                 },
+                {
+                    f"{field}.network_address": {
+                        "$gte": Int64(address.network_address)
+                    },
+                    f"{field}.broadcast_address": {
+                        "$lte": Int64(address.broadcast_address)
+                    },
+                },
             ],
         }
     else:
-        raise NotImplementedError("IPv6 address overlaps not implemented yet.")
+        net_high, net_low = struct.unpack(
+            ">qq", int(address.network_address).to_bytes(length=16)
+        )
+        net_high, net_low = Int64(net_high), Int64(net_low)
+
+        bcast_high, bcast_low = struct.unpack(
+            ">qq", int(address.broadcast_address).to_bytes(length=16)
+        )
+        bcast_high, bcast_low = Int64(bcast_high), Int64(bcast_low)
+
+        def lte_128(addr_prefix: str, target_high: Int64, target_low: Int64):
+            return {
+                "$or": [
+                    {f"{addr_prefix}.0": {"$lt": target_high}},
+                    {
+                        "$and": [
+                            {f"{addr_prefix}.0": target_high},
+                            {f"{addr_prefix}.1": {"$lte": target_low}},
+                        ],
+                    },
+                ],
+            }
+
+        def gte_128(addr_prefix: str, target_high: Int64, target_low: Int64):
+            return {
+                "$or": [
+                    {f"{addr_prefix}.0": {"$gt": target_high}},
+                    {
+                        "$and": [
+                            {f"{addr_prefix}.0": target_high},
+                            {f"{addr_prefix}.1": {"$gte": target_low}},
+                        ],
+                    },
+                ],
+            }
+
+        return {
+            f"{field}.version": 6,
+            "$or": [
+                {
+                    "$and": [
+                        lte_128(f"{field}.network_address", net_high, net_low),
+                        gte_128(f"{field}.network_address", bcast_high, bcast_low),
+                    ],
+                },
+                {
+                    "$and": [
+                        lte_128(f"{field}.broadcast_address", net_high, net_low),
+                        gte_128(f"{field}.broadcast_address", bcast_high, bcast_low),
+                    ],
+                },
+                {
+                    "$and": [
+                        lte_128(f"{field}.network_address", net_high, net_low),
+                        gte_128(f"{field}.broadcast_address", bcast_high, bcast_low),
+                    ],
+                },
+                {
+                    "$and": [
+                        gte_128(f"{field}.network_address", net_high, net_low),
+                        lte_128(f"{field}.broadcast_address", bcast_high, bcast_low),
+                    ],
+                },
+            ],
+        }
